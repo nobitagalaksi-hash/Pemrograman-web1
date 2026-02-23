fcfc45692e804fe8bb5279d4c78ee76ad85b73a2
ARYA PUTRA RAHMAN DHIKA <nobitagalaksi@gmail.com>
2026-02-23 09:23:34 +0700

    Add files via upload

---
+++ login_process.php
@@ -0,0 +1,8 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == 'admin' && 
+    $password == '123456') {
+  echo 'Login succeeds.';
+} else echo 'Login failed.';
---
+++ register_process.php
@@ -0,0 +1,4 @@
+<?php
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  echo "Registering $username with password $password";
